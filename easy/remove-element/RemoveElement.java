139b39def567ef51bef88adadec34e560cc945af
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-08-09 20:37:02 -0700

    add solution for remove-element in Java

---
+++ easy/remove-element/RemoveElement.java
@@ -0,0 +1,19 @@
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        System.out.println(removeElement(nums, 2));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
